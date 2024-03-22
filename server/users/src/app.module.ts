49d3745e11af3584c1a23b88acb0e5a40ff2fb5b
AMRRAGHEB118 <amrragheb118@gmail.com>
2024-03-22 11:54:30 +0200

    Create traveler module and rm test files

--- server/users/src/app.module.ts
+++ server/users/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from 'lib/database.module';
 import { UsersModule } from './users/users.module';
+import { TravelerModule } from './traveler/traveler.module';
 
 @Module({
-    imports: [DatabaseModule, UsersModule],
+    imports: [DatabaseModule, UsersModule, TravelerModule],
     controllers: [AppController],
     providers: [AppService],
 })
