806dadd1b6e1a7dcdcd476192ae1bf3c7a27308d
AMRRAGHEB118 <amrragheb118@gmail.com>
2024-03-22 11:52:42 +0200

    Create user module and set user table in DB

---
+++ server/users/src/users/entity/user.entity.ts
@@ -0,0 +1,38 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+} from 'typeorm';
+
+export enum UserRole {
+    ADMIN = 'admin',
+    TRAVELER = 'traveler',
+}
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('varchar', { unique: true })
+    username: string;
+
+    @Column('varchar', { unique: true })
+    email: string;
+
+    @Column('varchar', { select: false })
+    password: string;
+
+    @Column({ type: 'enum', enum: UserRole, default: UserRole.TRAVELER })
+    role: UserRole;
+
+    @Column({ nullable: true })
+    token: string;
+
+    @CreateDateColumn({ readonly: true, select: false })
+    createdAt: Date;
+
+    @CreateDateColumn({ select: false })
+    updatedAt: Date;
+}
