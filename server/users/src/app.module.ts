3f03a59abd191ca4ac503babd4b5edb34faf90dd
AMRRAGHEB118 <amrragheb118@gmail.com>
2024-03-27 21:10:07 +0200

    Add use cases diagram

--- server/users/src/app.module.ts
+++ server/users/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from 'lib/database.module';
 import { UsersModule } from './users/users.module';
 import { TravelerModule } from './traveler/traveler.module';
 
 @Module({
-    imports: [DatabaseModule, UsersModule, TravelerModule],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [DatabaseModule, UsersModule, TravelerModule],
 })
 export class AppModule {}
